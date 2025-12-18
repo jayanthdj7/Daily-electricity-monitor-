cd0f66d4b0e0317624a660858dd1345cdea0c73a
Jayanth D.J <jayanthdj.ch25@bmsce.ac.in>
2025-12-18 16:12:27 +0530

    Create output.c

---
+++ output.c
@@ -0,0 +1,61 @@
+===Daily Electricity Bill Calculator===
+
+Choice no.       Action
+   1.        Enter appliance details
+   2.        Log Usage
+   3.        View today's bill
+
+Enter the choice no. for the action to be performed: 1
+
+==APPLIANCE DETAILS==
+
+Enter the number of appliances: 2
+
+NOTE: Enter the acronyms for 'appliances name' (Ex:  WM for Washing Machine)
+
+Enter the name of appliance 1 (acronym): heater
+Enter the power rating (in watts) of heater: 250
+
+Enter the name of appliance 2 (acronym): tv
+Enter the power rating (in watts) of tv: 200
+
+
+Appliance details entered successfully.
+
+
+Choice no.   Action
+   1.        Enter appliance details
+   2.        Log Usage
+   3.        View today's bill
+
+Enter the choice no. for the action to be performed: 2
+
+==LOG USAGE==
+
+NOTE: If there are many units of same appliance, enter the sum of duration used.
+
+Enter the duration (in hours) for heater: 3
+heater consumed 0.75 kWh costing ₹4.50
+Enter the duration (in hours) for tv: 15
+tv consumed 3.00 kWh costing ₹18.00
+
+Choice no.   Action
+   1.        Enter appliance details
+   2.        Log Usage
+   3.        View today's bill
+
+Enter the choice no. for the action to be performed: 3
+
+==TODAY'S BILL==
+
+Total cost for the day : ₹ 22.50
+
+Number of free units per month as per Gruha Jyoti Sheme = 200
+Enter the month (01 to 12)   07
+Number of free units per day during this month = 200/31 = 6.45
+Subsidy amount per day during this month =₹ 38.71
+
+Congratulations, you've consumed 2.70 units less than the subsidy limit
+Keep it up
+Amount to be paid for the day = ₹ 0.0 rupees
+Exiting...
